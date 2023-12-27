087bce3efb71795f1349c6114224d2b435c248bb
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2023-12-27 22:38:40 +0530

    Update baseUrl.js

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://elegant.vercel.app' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'http://localhost:3000';
 export default baseUrl;
