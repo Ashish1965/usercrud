a6eab67006d1370f5556e012709ef8233e6458b0
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2023-12-28 00:26:58 +0530

    Update dropdown.js

--- components/dropdown.js
+++ components/dropdown.js
@@ -15,7 +15,7 @@ function Dropdown({ options, value, onChange }) {
 
   return (
     <div className='float-left overflow-hidden'>
-      <button onClick={handleClick}><input type="text" value={value} placeholder='Select Your State' className='border w-full h-10 mt-4 p-6 rounded-md'/></button>
+      <button onClick={handleClick}><input type="text" value={value} name='userState' id='UseState' placeholder='Select Your State' className='border w-full h-10 mt-4 p-6 rounded-md'/></button>
       {isOpen && (
         <div className='absolute border rounded-md bg-white p-5'>
         <ul>
